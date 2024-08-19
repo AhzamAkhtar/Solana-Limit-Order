eec3bc514c6bafe7132033e692635615a5a5985a
Ahzam Akhtar <ahzam8186@gmail.com>
2024-08-19 18:43:23 +0530

    Added Different Test Cases

--- tests/cega.ts
+++ tests/cega.ts
@@ -5,6 +5,7 @@ import * as bs58 from "bs58";
 import { BN } from "@coral-xyz/anchor";
 import { PublicKey, Commitment, Keypair, SystemProgram } from "@solana/web3.js";
 import { randomBytes } from "crypto";
+import { ASSOCIATED_PROGRAM_ID } from "@coral-xyz/anchor/dist/cjs/utils/token";
 import {
   ASSOCIATED_TOKEN_PROGRAM_ID as associatedTokenProgram,
   TOKEN_PROGRAM_ID as tokenProgram,
@@ -12,16 +13,17 @@ import {
   createAccount,
   mintTo,
   getAssociatedTokenAddress,
-  TOKEN_PROGRAM_ID, ASSOCIATED_TOKEN_PROGRAM_ID
-} from "@solana/spl-token"
-const commitment: Commitment = "confirmed"; 
+  TOKEN_PROGRAM_ID,
+  ASSOCIATED_TOKEN_PROGRAM_ID,
+} from "@solana/spl-token";
+const commitment: Commitment = "confirmed";
 
 describe("cega solana assignment", () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
 
   const programId = new PublicKey(
-    "8YapqrY61XUSMUw6bCNK9kogG65mxM2j8PpRw4jaf8Vx"
+    "CAxcPk6uf5a92YswtUVB94LCWD3sUJinpYJ8qLdpPCZA"
   );
   const program = new anchor.Program<Cega>(
     IDL,
@@ -30,7 +32,7 @@ describe("cega solana assignment", () => {
   );
 
   const seller = Keypair.fromSecretKey(bs58.decode(wallet));
-  const buyer = Keypair.fromSecretKey(bs58.decode(wallet_two))
+  const buyer = Keypair.fromSecretKey(bs58.decode(wallet_two));
   // Random seed
   const seed = new BN(randomBytes(8));
 
@@ -56,16 +58,140 @@ describe("cega solana assignment", () => {
   let vault_x_ata: PublicKey;
 
   it("Create mints, tokens and ATAs", async () => {
-    mint_x = new PublicKey("Gh9ZwEmdLJ8DscKNTkTqPbNwLNNBjuSzaG9Vp2KGtKJr")
-    mint_usdc = new PublicKey("")
-    seller_x_ata = await getAssociatedTokenAddress(mint_x, seller.publicKey, false, tokenProgram)
-    seller_usdc_ata = await getAssociatedTokenAddress(mint_usdc, seller.publicKey, false, tokenProgram);
-    buyer_x_ata = await getAssociatedTokenAddress(mint_x, buyer.publicKey, false, tokenProgram);
-    buyer_usdc_ata = await getAssociatedTokenAddress(mint_usdc, buyer.publicKey, false, tokenProgram);
+    mint_x = new PublicKey("Gh9ZwEmdLJ8DscKNTkTqPbNwLNNBjuSzaG9Vp2KGtKJr");
+    mint_usdc = new PublicKey("");
+    seller_x_ata = await getAssociatedTokenAddress(
+      mint_x,
+      seller.publicKey,
+      false,
+      tokenProgram
+    );
+    seller_usdc_ata = await getAssociatedTokenAddress(
+      mint_usdc,
+      seller.publicKey,
+      false,
+      tokenProgram
+    );
+    buyer_x_ata = await getAssociatedTokenAddress(
+      mint_x,
+      buyer.publicKey,
+      false,
+      tokenProgram
+    );
+    buyer_usdc_ata = await getAssociatedTokenAddress(
+      mint_usdc,
+      buyer.publicKey,
+      false,
+      tokenProgram
+    );
     // Create take ATAs
-    vault_x_ata = await getAssociatedTokenAddress(mint_x, auth, true, tokenProgram);
+    vault_x_ata = await getAssociatedTokenAddress(
+      mint_x,
+      auth,
+      true,
+      tokenProgram
+    );
+  });
 
-})
+  it("Initialize", async () => {
+    try {
+      const tx = await program.methods
+        .initialize(
+          seed,
+          new BN(600000),
+          seller.publicKey,
+          new BN(100),
+          new BN(10)
+        )
+        .accounts({
+          auth,
+          user: seller.publicKey,
+          mintX: mint_x,
+          vaultX: vault_x_ata,
+          config,
+          tokenProgram: TOKEN_PROGRAM_ID,
+          associatedTokenProgram: ASSOCIATED_PROGRAM_ID,
+          systemProgram: SystemProgram.programId,
+        })
+        .signers([seller])
+        .rpc({ skipPreflight: true });
+      await confirmTx(tx);
+      console.log("Your transaction signature", tx);
+    } catch (e) {
+      console.error(e);
+    }
+  });
+
+  it("Transfer Selling Token To Valut", async () => {
+    try {
+      const tx = await program.methods
+        .transferTokenToVault()
+        .accountsStrict({
+          auth,
+          seller: seller.publicKey,
+          mintX: mint_x,
+          sellerVaultX: seller_x_ata,
+          vaultX: vault_x_ata,
+          config,
+          tokenProgram: TOKEN_PROGRAM_ID,
+          associatedTokenProgram: ASSOCIATED_TOKEN_PROGRAM_ID,
+          systemProgram: SystemProgram.programId,
+        })
+        .signers([seller])
+        .rpc({ skipPreflight: true });
+      await confirmTx(tx);
+      console.log("Your transaction signature", tx);
+    } catch (e) {
+      let err = e as anchor.AnchorError;
+      console.error(err);
+    }
+  });
+
+  it("Transfer Token To Buyer and Make the Trade", async () => {
+    try {
+      const tx = await program.methods
+        .transferTokenToBuyer()
+        .accountsStrict({
+          auth,
+          seller: seller.publicKey,
+          buyer: buyer.publicKey,
+          mintX: mint_x,
+          mintUsdc: mint_usdc,
+          vaultX: vault_x_ata,
+          buyerVaultX: buyer_x_ata,
+          buyerVaultUsdc: buyer_usdc_ata,
+          sellerVaultUsdc: seller_usdc_ata,
+          config,
+          tokenProgram: TOKEN_PROGRAM_ID,
+          associatedTokenProgram: ASSOCIATED_TOKEN_PROGRAM_ID,
+          systemProgram: SystemProgram.programId,
+        })
+        .signers([buyer])
+        .rpc({ skipPreflight: true });
+      await confirmTx(tx);
+      console.log("Your transaction signature", tx);
+    } catch (e) {
+      let err = e as anchor.AnchorError;
+      console.error(err);
+    }
+  });
+
+  it("Update", async () => {
+    try {
+      const tx = await program.methods
+        .update(new BN(120), new BN(60000000))
+        .accountsStrict({
+          config,
+        })
+        .signers([buyer])
+        .rpc({ skipPreflight: true });
+      await confirmTx(tx);
+      console.log("Your transaction signature", tx);
+    } catch (e) {
+      let err = e as anchor.AnchorError;
+      console.error(err);
+    }
+  });
 
   const confirmTx = async (signature: string) => {
     const latestBlockhash = await anchor
@@ -80,7 +206,4 @@ describe("cega solana assignment", () => {
     );
   };
 
-  const confirmTxs = async (signatures: string[]) => {
-    await Promise.all(signatures.map(confirmTx));
-  };
 });
