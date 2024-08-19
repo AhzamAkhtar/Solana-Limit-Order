dd6a8fcb39223c8b5b85ca1448863c339fe189e8
Ahzam Akhtar <ahzam8186@gmail.com>
2024-08-19 21:49:33 +0530

    Refactor

--- programs/cega/src/state/config.rs
+++ programs/cega/src/state/config.rs
@@ -3,19 +3,19 @@ use anchor_lang::prelude::*;
 #[account]
 
 pub struct Config {
-    pub seed: u64,
-    pub authority: Option<Pubkey>,
-    pub mint_x: Pubkey,
-    pub auth_bump: u8,
-    pub config_bump: u8,
-    pub expiry: u64,
-    pub amount : u64,
-    pub price : u64,
-    pub amount_preserve : u64,
+    pub seed: u64,                 //8
+    pub authority: Option<Pubkey>, //(1+32)
+    pub mint_x: Pubkey,            //32
+    pub auth_bump: u8,             //1
+    pub config_bump: u8,           //1
+    pub expiry: u64,               //8
+    pub amount: u64,               //8
+    pub price: u64,                //8
+    pub amount_preserve: u64,      //8
 }
 
 impl Config {
-    pub const LEN: usize = 8 + 8 + (1 + 32) + 32 + (2 * 1);
+    pub const LEN: usize = 8 + 8 + (1 + 32) + 32 + (2 * 1) + (4 * 8);
 
     pub fn check_expiry(&self) -> Result<()> {
         require!(
@@ -24,5 +24,4 @@ impl Config {
         );
         Ok(())
     }
-
 }
